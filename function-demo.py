8b8331df1915c002791b6f9b14e965614371afd6
kitri <kim@a.a>
2020-07-09 13:58:54 +0900

    initial commit

---
+++ function-demo.py
@@ -0,0 +1,12 @@
+def add_two_number(first=0, second=0, third=0):
+    return first + second + third
+
+
+def main():
+    num_one = 3
+    num_two = 4
+    result = add_two_number(3, third=3)
+    print(result)
+
+
+main()
--- melon-selenium.py
+++ melon-selenium.py
@@ -8,49 +8,92 @@ import time
 import pandas as pd
 import requests
 
-options = webdriver.ChromeOptions()
-# options.add_argument('headless')
-# options.add_argument('window-size=1920x1080')
 
-browser = webdriver.Chrome('./chromedriver.exe', options=options)
+def save_to_csv(tabular_data=[], file_name='naname.csv'):
+    df = pd.DataFrame(tabular_data)
+    df.to_csv(file_name, index=False)
+    print('save ok..')
 
-browser.get('https://www.melon.com/')
 
-browser.find_element(By.CSS_SELECTOR, '#gnb_menu > ul:nth-child(1) > li.nth1 > a > span.menu_bg.menu01').click()
+def initialize_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
 
-WebDriverWait(browser, timeout=5)\
-    .until(EC.presence_of_all_elements_located)
-time.sleep(1)
-browser.find_element(By.CSS_SELECTOR, '#gnb_menu > ul:nth-child(1) > li.nth2 > a > span.menu_bg.menu02').click()
+    browser = webdriver.Chrome('./chromedriver.exe', options=options)
+    print('browser created..')
+    return browser
 
-time.sleep(1)
-# browser.get_screenshot_as_file('./screenshot/melon.png')
 
-html = browser.page_source
+def parse_page_and_return_soup(page, browser):
 
-# 2. 페이지에서 데이터 파싱
-soup = BeautifulSoup(html, 'html.parser')
-browser.close()
+    if page == 1:
+        print('page: ' + str(page) + ' parsing')
+        browser.get('https://www.melon.com/')
 
-melon_chart = []
-for tr in soup.select('#frm > div > table > tbody > tr'):
-    # 이미지 저장..
-    res = requests.get(tr.select_one('tr > td:nth-child(3) > div > a > img')['src'])
-    file_name = tr.select_one('tr > td:nth-child(3) > div > a > img')['src'].split('/')[-7]
-    with open('./melon_images/' + file_name, 'wb') as file:
-        file.write(res.content)
+        browser.find_element(By.CSS_SELECTOR, '#gnb_menu > ul:nth-child(1) > li.nth1 > a > span.menu_bg.menu01').click()
 
-    melon_chart.append([
-        tr.select_one('tr > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a').text,
-        tr.select_one('tr > td:nth-child(5) > div > div > div.ellipsis.rank02 > a').text,
-        tr.select_one('tr > td:nth-child(6) > div > div > div > a').text,
-        int(tr.select_one('tr > td:nth-child(7) > div > button > span.cnt')
-            .text.replace('총건수', '').strip().replace(',', '')),
-        file_name
-    ])
+        WebDriverWait(browser, timeout=5) \
+            .until(EC.presence_of_all_elements_located)
+        time.sleep(1)
+        browser.find_element(By.CSS_SELECTOR, '#gnb_menu > ul:nth-child(1) > li.nth2 > a > span.menu_bg.menu02').click()
 
-import pprint
-pprint.pprint(melon_chart)
-# 3. 저장
-df = pd.DataFrame(melon_chart)
-df.to_csv('melon_chart.csv', index=False)
+        time.sleep(1)
+        # browser.get_screenshot_as_file('./screenshot/melon.png')
+
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
+    else:
+        time.sleep(1)
+        print('page: ' + str(page) + ' parsing')
+        browser.\
+            find_element(By.CSS_SELECTOR,
+                         '#pageObjNavgation > div > span > a:nth-child(' + str(page) + ')')\
+            .click()
+        time.sleep(1)
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
+
+
+def get_melon_data_with_soup(soup):
+    melon_chart = []
+    for tr in soup.select('#frm > div > table > tbody > tr'):
+        # 이미지 저장..
+        res = requests.get(tr.select_one('tr > td:nth-child(3) > div > a > img')['src'])
+        file_name = tr.select_one('tr > td:nth-child(3) > div > a > img')['src'].split('/')[-7]
+        with open('./melon_images/' + file_name, 'wb') as file:
+            file.write(res.content)
+
+        melon_chart.append([
+            tr.select_one('tr > td:nth-child(2) > div > span.rank').text.strip(),
+            tr.select_one('tr > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a').text,
+            tr.select_one('tr > td:nth-child(5) > div > div > div.ellipsis.rank02 > a').text,
+            tr.select_one('tr > td:nth-child(6) > div > div > div > a').text,
+            int(tr.select_one('tr > td:nth-child(7) > div > button > span.cnt')
+                .text.replace('총건수', '').strip().replace(',', '')),
+            file_name
+        ])
+
+    return melon_chart
+
+
+def main():
+    melon_chart = []
+
+    browser = initialize_browser()
+
+    for page in range(1, 11):
+        soup = parse_page_and_return_soup(page, browser)
+        # 2. 페이지에서 데이터 파싱
+        partial_chart = get_melon_data_with_soup(soup)
+        melon_chart.extend(partial_chart)
+
+    browser.close()
+
+    # 3. 저장
+    save_to_csv(melon_chart, 'melon_chart.csv')
+
+
+main()
